7ee747f6bf7adaee22f8d1c1d815416d8bd7fecf
Kenny Mui <klmui@wisc.edu>
2020-02-13 00:41:38 -0600

    Finish milestone 2

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.lab3milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
+import com.example.lab3milestone2.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // get the reference of Button's
+        firstFragment =(Button)findViewById(R.id.firstFragment);
+        secondFragment =(Button)findViewById(R.id.secondFragment);
+
+        // perform setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load First Fragment
+                loadFragment(new FirstFragment());
+            }
+        });
+        // Perform setonClickListener event on Second Button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               // load second Fragment
+               loadFragment(new SecondFragment());
+           }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+        // Create a FragmentTransaction to begin the transaction and replace the fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // Replace the FrameLayout with the new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 }
